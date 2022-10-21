39d2c57e4919c6a46c50daca2a3272e0f54b9484
Justin-David-Garcia <Justin.d.garcia@gmail.com>
2022-10-20 18:42:34 -0600

    docs: update Bikeshare_2.py docstrings
    Delete Hint Docstrings provided by Udacity.

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -36,7 +36,7 @@ def get_filters():
     print('Hello! Let\'s explore some US bikeshare data!')
     print('The options for cities are:')
     print('Chicago, New York City, Washington')
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # get user input for city (chicago, new york city, washington).
     while True:
         try:
             city = str(input('Choose a city for analysis:   ')).lower()
